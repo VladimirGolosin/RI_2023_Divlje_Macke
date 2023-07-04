2995c443dd1240b0321cd88d7811d72eb323953a
VladimirGolosin <96448683+VladimirGolosin@users.noreply.github.com>
2023-07-04 13:48:09 +0200

    dodato čuvanje modela i slika

--- convolutional neural network/draw.py
+++ convolutional neural network/draw.py
@@ -5,13 +5,17 @@ import math
 from sklearn.metrics import confusion_matrix
 
 
-def draw_graphs(history):
+def draw_graphs(history, export_dir=None):
     plt.figure(figsize=(10, 5))
     plt.plot(range(1, len(history.history['loss']) + 1), history.history['loss'], label='Train Loss')
     plt.plot(range(1, len(history.history['val_loss']) + 1), history.history['val_loss'], label='Valid Loss')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend()
+
+    if export_dir:
+        plt.savefig(f'{export_dir}/loss.png')
+
     plt.show()
 
     plt.figure(figsize=(10, 5))
@@ -20,23 +24,21 @@ def draw_graphs(history):
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
     plt.legend()
+
+    if export_dir:
+        plt.savefig(f'{export_dir}/accuracy.png')
+
     plt.show()
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-import math
-from sklearn.metrics import confusion_matrix
 
-
-def draw_confusion_matrix(true_labels, pred_labels, class_labels):
+def draw_confusion_matrix(true_labels, pred_labels, class_labels, export_dir=None):
     confusion_mat = confusion_matrix(true_labels, pred_labels)
     plt.figure(figsize=(10, 8))
     sns.heatmap(confusion_mat, annot=True, fmt='d', cmap='Blues', xticklabels=class_labels, yticklabels=class_labels)
     plt.xlabel('Predicted Type')
     plt.ylabel('True Type')
-    plt.xticks(rotation=30)  # Rotate x-axis labels for better visibility
-    plt.yticks(rotation=0)  # Keep y-axis labels horizontal
+    plt.xticks(rotation=30)
+    plt.yticks(rotation=0)
 
     # Add explicit labels
     ax = plt.gca()
@@ -45,10 +47,13 @@ def draw_confusion_matrix(true_labels, pred_labels, class_labels):
     plt.xlabel('Predicted Type', fontsize=12, fontweight='bold')
     plt.ylabel('True Type', fontsize=12, fontweight='bold')
 
+    if export_dir:
+        plt.savefig(f'{export_dir}/confusion.png')
+
     plt.show()
 
 
-def plot_predictions(images, true_labels, pred_labels, class_labels):
+def plot_predictions(images, true_labels, pred_labels, class_labels, export_dir=None):
     num_images = len(images)
     num_rows = math.ceil(num_images / 5)
     num_cols = min(num_images, 5)
@@ -74,5 +79,7 @@ def plot_predictions(images, true_labels, pred_labels, class_labels):
 
             axs[i, j].set_title(f'True: {true_label}\nPred: {pred_label}', color=color)
 
-    plt.show()
+    if export_dir:
+        plt.savefig(f'{export_dir}/predictions.png')
 
+    plt.show()
--- convolutional neural network/main.py
+++ convolutional neural network/main.py
@@ -11,7 +11,6 @@ from draw import draw_graphs, draw_confusion_matrix, plot_predictions
 import math
 from sklearn.metrics import confusion_matrix
 
-
 def main():
     base_dir = os.path.dirname(os.path.abspath(__file__))
     data_dir = os.path.join(base_dir, '..', 'data')
@@ -27,7 +26,7 @@ def main():
     input_shape = (224, 224, 3)
 
     batch_size = 64
-    epochs = 20
+    epochs = 4
 
     train_datagen = ImageDataGenerator(
         rescale=1.0 / 255,
@@ -63,25 +62,36 @@ def main():
         shuffle=False
     )
 
-    model = CatClassifier(num_classes, input_shape)
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    export_dir = os.path.join(base_dir, 'export')
+    export_model_file = os.path.join(export_dir, 'cat_classifier_model')
+    model_dir = os.path.join(base_dir, 'model')
+    model_file = os.path.join(model_dir, 'cat_classifier_model')
 
-    history = model.fit(
-        train_generator,
-        steps_per_epoch=train_generator.samples // batch_size,
-        epochs=epochs,
-        validation_data=valid_generator,
-        validation_steps=valid_generator.samples // batch_size
-    )
+    if os.path.exists(model_file):
+        print("Loading pre-existing model from the model folder...")
+        model = tf.keras.models.load_model(model_file)
+    else:
+        print("Training the model...")
+        model = CatClassifier(num_classes, input_shape)
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        history = model.fit(
+            train_generator,
+            steps_per_epoch=train_generator.samples // batch_size,
+            epochs=epochs,
+            validation_data=valid_generator,
+            validation_steps=valid_generator.samples // batch_size
+        )
+        os.makedirs(export_dir, exist_ok=True)
+        model.save(export_model_file, save_format='tf')
+
+        draw_graphs(history, export_dir)
 
     test_loss, test_accuracy = model.evaluate(test_generator)
     print(f'Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.2f}%')
 
-    draw_graphs(history)
-
     test_predictions = model.predict(test_generator)
     test_pred_labels = np.argmax(test_predictions, axis=1)
-    draw_confusion_matrix(test_generator.labels, test_pred_labels, class_labels)
+    draw_confusion_matrix(test_generator.labels, test_pred_labels, class_labels, export_dir)
 
     print("Generating predictions for random images...")
 
@@ -94,7 +104,7 @@ def main():
         image_path = os.path.join(test_dir, test_generator.filenames[i])
         image = tf.keras.preprocessing.image.load_img(image_path, target_size=input_shape[:2])
         image = tf.keras.preprocessing.image.img_to_array(image)
-        image = image / 255.0  # Normalize image
+        image = image / 255.0
         subset_images.append(image)
 
         true_label = test_generator.labels[i]
@@ -104,7 +114,7 @@ def main():
         subset_true_labels.append(true_label)
         subset_pred_labels.append(pred_label)
 
-    plot_predictions(subset_images, subset_true_labels, subset_pred_labels, class_labels)
+    plot_predictions(subset_images, subset_true_labels, subset_pred_labels, class_labels, export_dir)
 
 
 if __name__ == '__main__':
